598230df4e36ad3500eeb3b809ec4a92dd211d85
tslai <tl@quartile.co>
2017-12-04 10:46:28 +0000

    [FIX] use product_id field

--- sale_ext_sst/models/sale_order.py
+++ sale_ext_sst/models/sale_order.py
@@ -12,15 +12,3 @@ class SaleOrder(models.Model):
         related='partner_id.phone',
         string='Phone',
     )
-    order_item = fields.Many2one(
-        'product.product',
-        string='Product',
-        compute='_get_first_product',
-    )
-
-    @api.depends('order_line')
-    def _get_first_product(self):
-        for order in self:
-            for order_line in order:
-                order.order_item = order_line.product_id
-                break
