bc119e414441db8a69e300cdb2f9c74f420a94d4
Tim <tl@quartile.co>
2017-12-06 14:45:47 +0800

    ピッキングリストビュー調整（プロダクト・運送会社表示） (#23)

---
+++ stock_ext_sst/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Extension for inventory functions',
+    'version': '11.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Stock',
+    'license': "AGPL-3",
+    'description': "",
+    'depends': [
+        'stock',
+        'delivery',
+    ],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'installable': True,
+}
