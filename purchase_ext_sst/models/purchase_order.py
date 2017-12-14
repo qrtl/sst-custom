e55b49ce0be8f2fc95c371a833043e6a22f1583f
tslai <tl@quartile.co>
2017-12-14 09:28:53 +0000

    [IMP] Add fields and domain filter to purchase order

--- purchase_ext_sst/models/purchase_order.py
+++ purchase_ext_sst/models/purchase_order.py
@@ -20,16 +20,55 @@ class PurchaseOrder(models.Model):
     phone = fields.Char(index=True)
     address = fields.Char()
     remark = fields.Text('Remark')
+    delivery_staff = fields.Many2one('res.partner', 'Delivery Staff')
+    worked_hours = fields.Selection(
+        [(0.5, '30 minutes'),
+         (1, '1 hour'),
+         (1.5, '1.5 hours'),
+         (2, '2 hours'),
+         (2.5, '2.5 hours'),
+         (3, '3 hours'),
+         (3.5, '3.5 hours'),
+         (4, '4 hours'),
+         (4.5, '4.5 hours'),
+         (5, '5 hours'),
+         (5.5, '5.5 hours'),
+         (6, '6 hours'),
+         (6.5, '6.5 hours'),
+         (7, '7 hours'),
+         (7.5, '7.5 hours'),
+         (8, '8 hours'),
+         (8.5, '8.5 hours'),
+         (9, '9 hours'),
+         (9.5, '9.5 hours'),
+         (10, '10 hours'),],
+        string='Worked Hours'
+    )
 
-
-    @api.onchange('partner_id', 'company_id')
-    def onchange_partner_id(self):
-        super(PurchaseOrder, self).onchange_partner_id()
-        if not self.partner_id:
-            self.shop_id = False
-        else:
-            self.shop_id = self.partner_id.shop_id
+    @api.onchange('delivery_staff')
+    def onchange_delivery_staff(self):
+        if (not self.shop_id and self.delivery_staff) or \
+            (self.shop_id and self.delivery_staff and \
+                self.delivery_staff.shop_id != self.shop_id):
+            self.shop_id = self.delivery_staff.shop_id
         return {}
+
+    @api.onchange('shop_id')
+    def onchange_shop_id(self):
+        ids = []
+        if self.shop_id:
+            # Update domain filter on delivery staff
+            staffs = self.env['res.partner'].search([
+                ('shop_id', '=', self.shop_id.id)
+            ])
+            ids.append(('id', 'in', staffs.ids))
+            # Clear the delivery staff value
+            if self.delivery_staff and self.delivery_staff.shop_id and \
+                    self.delivery_staff.shop_id != self.shop_id:
+                self.delivery_staff = False
+        return {
+            'domain': {'delivery_staff': ids}
+        }
 
     @api.multi
     def open_record(self):
