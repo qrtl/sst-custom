c8438a9c8567febd1e966a71b89bb452ed8a2f89
tslai <tl@quartile.co>
2017-12-18 09:34:06 +0000

    [FIX] worked_hours definition

--- purchase_ext_sst/models/purchase_order.py
+++ purchase_ext_sst/models/purchase_order.py
@@ -22,7 +22,11 @@ class PurchaseOrder(models.Model):
     remark = fields.Text('Remark')
     delivery_staff_id = fields.Many2one('hr.employee', 'Delivery Staff')
     worked_hours = fields.Selection(
-        [(num/2, str(num/2) + ' hours') for num in range(1, 21)],
+        [(num, num + ' hours') for num in ['0.5', '1.0', '1.5', '2.0',
+                                           '2.5', '3.0', '3.5', '4.0',
+                                           '4.5', '5.0', '5.5', '6.0',
+                                           '6.5', '7.0', '7.5', '8.0',
+                                           '8.5', '9.0', '9.5', '10.0']],
         string='Worked Hours',
     )
 
