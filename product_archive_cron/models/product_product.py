59401d7c0ce3a356790e76449297a08ba8a0c683
tslai <tl@quartile.co>
2017-12-28 09:50:10 +0000

    [FIX] SQL and archive product.template

--- product_archive_cron/models/product_product.py
+++ product_archive_cron/models/product_product.py
@@ -13,34 +13,43 @@ class ProductProduct(models.Model):
         product_obj = self.env['product.product']
 
         self.env.cr.execute("""
+            WITH
+                purchase_info AS(
+                    SELECT
+                        pol.product_qty,
+                        pol.product_id
+                    FROM
+                        purchase_order po
+                    LEFT JOIN purchase_order_line pol
+                        ON (pol.order_id = po.id)
+                    WHERE
+                        po.state = 'draft'
+                )
             SELECT
-                SUM(pol.product_qty) as product_qty,
-                pr.id as product_id
+                SUM(pi.product_qty) as product_qty,
+                pp.id as product_id
             FROM
-                purchase_order po
-            LEFT JOIN purchase_order_line pol
-                ON (pol.order_id = po.id)
-            LEFT JOIN product_product pr
-                ON (pol.product_id = pr.id)
+                product_product pp
             LEFT JOIN product_template pt
-                ON (pr.product_tmpl_id = pt.id)
+                ON (pp.product_tmpl_id = pt.id)
+            LEFT JOIN purchase_info pi
+                ON (pi.product_id = pp.id)
             WHERE
                 pt.type = 'product' AND
-                po.state = 'draft' AND
-                pr.active IS TRUE
+                pp.active IS TRUE
             GROUP BY
-                pr.id
-            """)
+                pp.id
+        """)
 
         query_result = self.env.cr.dictfetchall()
         for result in query_result:
             product = product_obj.browse(result['product_id'])
             virtual_available = product.virtual_available
-            product_qty = result['product_qty']
+            product_qty = result['product_qty'] or 0
             if virtual_available + product_qty < 0.0:
                 # if product has reordering rules then deactivate first
                 if "orderpoint_ids" in product._fields:
                     if product.orderpoint_ids:
                         product.orderpoint_ids.write({'active': False})
                 # deactivate product
-                product.write({'active': False})
+                product.product_tmpl_id.write({'active': False})
