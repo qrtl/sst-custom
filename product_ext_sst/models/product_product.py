fdb6e5bc53f68c57a2f7ce2e23f2ecc21716d90e
tslai <tl@quartile.co>
2018-01-03 08:14:52 +0000

    [FIX] Use compute field, update comments

--- product_ext_sst/models/product_product.py
+++ product_ext_sst/models/product_product.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Quartile Limited
+# Copyright 2017-2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api
@@ -8,15 +8,12 @@ from odoo import models, api
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    @api.multi
-    def write(self, vals):
-        if vals.get('default_code') and vals['default_code'].isdigit():
-            vals['barcode'] = vals['default_code']
-        return super(ProductProduct, self).write(vals)
+    barcode = fields.Char(
+        compute="_get_barcode",
+        store=True,
+    )
 
-    @api.model
-    def create(self, vals):
-        res = super(ProductProduct, self).create(vals)
-        if res.default_code and res.default_code.isdigit():
-            res.barcode = res.default_code
-        return res
+    @api.depends('default_code')
+    def _get_barcode(self):
+        for pp in self:
+            pp.barcode = pp.default_code
