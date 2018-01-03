0a438163c4c2cdbb2b3ded6d0abd97a653d96527
tslai <tl@quartile.co>
2018-01-03 08:17:58 +0000

    [FIX] Adjust import

--- product_ext_sst/models/product_product.py
+++ product_ext_sst/models/product_product.py
@@ -2,7 +2,7 @@
 # Copyright 2017-2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import models, fields, api
 
 
 class ProductProduct(models.Model):
