0aaa5666b74507f36ddf26ae3727a89231dea419
tslai <tl@quartile.co>
2018-01-03 07:34:59 +0000

    [IMP] Use tax excluded price for the sale_price_unit

--- product_sales_record/models/sale_order.py
+++ product_sales_record/models/sale_order.py
@@ -14,7 +14,7 @@ class SaleOrder(models.Model):
         for order_line in self.order_line:
             product = order_line.product_id.product_tmpl_id
             product.write({
-                'sale_price_unit': order_line.price_unit,
+                'sale_price_unit': order_line.price_reduce_taxexcl,
                 'confirmation_date': self.confirmation_date,
                 'team_id': self.team_id.id,
             })
