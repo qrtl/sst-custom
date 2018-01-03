4347d88e5b14a18e1713ab313f3c38afbb40d499
Tim <tl@quartile.co>
2018-01-03 19:27:49 +0800

    商品アーカイブ処理 (#40)

---
+++ product_archive_cron/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Product Archive Cron',
+    'version': '11.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Extra Tools',
+    'license': "AGPL-3",
+    'description': """
+A cron task that will "Archive" products which have met certain conditions.
+    - Target products to "Archive": The product has to be a Stockable product
+    and (virtual_available + quantity requested in draft purchase.order) <= 0
+    """,
+    'summary': """Make product archive if product virtual quantity
+    +  purchase quantity is less than or equal to zero""",
+    'depends': [
+        'purchase'
+    ],
+    'data': [
+        'data/cron_data.xml',
+    ],
+    'installable': True,
+}
---
+++ product_archive_cron/models/product_product.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.model
+    def _process_product_archive(self):
+        self.env.cr.execute("""
+            SELECT
+                product_id,
+                sum(product_qty) AS purch_qty
+            FROM
+                purchase_order_line
+            WHERE
+                state = 'draft'
+            GROUP BY
+                product_id
+        """)
+        purch_dict = self.env.cr.dictfetchall()
+
+        purch_vals = {}
+        for dict in purch_dict:
+            purch_vals[dict['product_id']] = dict['purch_qty']
+
+        for prod in self.search(
+                [('type', '=', 'product'), ('active', '=', True)]):
+            if prod.virtual_available + purch_vals.get(prod.id, 0) <= 0:
+                # if product has reordering rules then deactivate first
+                if prod.orderpoint_ids:
+                    prod.orderpoint_ids.write({'active': False})
+                prod.product_tmpl_id.write({'active': False})
