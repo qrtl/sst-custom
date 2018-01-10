7ecdd6417abaf2bcacebfb6832308309d3f85a63
Tim <tl@quartile.co>
2018-01-10 12:27:22 +0800

    オンラインショップにつきカスタマイズ (#48)

---
+++ website_sale_adj/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Sales Adjustment',
+    'version': '11.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Product',
+    'license': "AGPL-3",
+    'description': """
+This module modify the website_sale module and provide following feature(s):
+ - Show product_state_id in the /shop/product page.
+ - Hide the "Shop" menu in header from public user
+ - Hide the /shop and /shop/product pages from public user
+    """,
+    'depends': [
+        'website_sale',
+        'product_ext_sst',
+    ],
+    'data': [
+        'views/templates.xml',
+        'security/website_sale_security.xml',
+    ],
+    'installable': True,
+}
