0218a2850ebb84742a9ac46b5fd460c5b3b89bc6
Mustufa Rangwala <mustufa.2007@gmail.com>
2018-01-12 11:39:12 +0530

    FIX stock_quant_sale_order -> Feedback fixs

--- stock_quant_sale_order/wizard/stock_quant_sale_order.py
+++ stock_quant_sale_order/wizard/stock_quant_sale_order.py
@@ -17,32 +17,34 @@ class QuantSaleOrderWizard(models.TransientModel):
 
     @api.multi
     def action_stock_quant_saleorder(self):
-        '''This methos create a sale order.'''
+        '''This method create a sale order.'''
         context = dict(self._context or {})
-        stock_quant_obj = self.env['stock.quant']
-        sale_order_obj = self.env['sale.order']
-        sale_order_line_obj = self.env['sale.order.line']
-
         active_ids = context.get('active_ids', [])
+        stock_quant_obj = self.env['stock.quant']
         quant_ids = stock_quant_obj.browse(active_ids)
         source_location = quant_ids[0].location_id
-
         if any(q.location_id != source_location for q in quant_ids):
             raise UserError(_('Please select quants that are in the same '
                               'Location.'))
-
-        if any(q.sale_order_id != False for q in quant_ids):
-            raise UserError(_('Some of the selected quants are already create sale order.'))
-
+        if any(q.sale_order_id for q in quant_ids):
+            raise UserError(
+                _('Some of the selected quants are already create sale order.')
+            )
+        if any(
+            self .env.user.company_id.id != q.company_id.id for q in quant_ids
+        ):
+            raise UserError(
+                _('You can not create sales order for different company.')
+            )
+        sale_order_obj = self.env['sale.order']
+        sale_order_line_obj = self.env['sale.order.line']
         origin_name = ','.join([q.display_name for q in quant_ids])
-
         order_vals = {
             'partner_id': self.partner_id.id,
             'state': 'draft',
             'origin': origin_name,
         }
         sale_order = sale_order_obj.sudo().create(order_vals)
-
         sale_order.onchange_partner_id()
 #         sale_order.onchange_partner_id_carrier_id()
 
