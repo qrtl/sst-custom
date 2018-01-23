bda794ddcbda8b9a6bd20ad4b3841b72cc293f56
tslai <tl@quartile.co>
2018-01-23 08:52:52 +0000

    [FIX] Change shop_id into Many2one

--- account_ext_sst/models/account_invoice_line.py
+++ account_ext_sst/models/account_invoice_line.py
@@ -2,21 +2,24 @@
 # Copyright 2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import models, fields, api
 
 
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
+    shop_id = fields.Many2one(
+        related='invoice_id.shop_id',
+        string='Shop',
+        readonly=True,
+    )
+
     @api.model
     def create(self, vals):
         res = super(AccountInvoiceLine, self).create(vals)
         if res.sale_line_ids:
-            shop_ids = []
             for order_line in res.sale_line_ids:
-                if order_line.order_id.warehouse_id.id and \
-                                order_line.order_id.warehouse_id.id not in \
-                                res.invoice_id.shop_ids.ids:
-                    shop_ids.append(order_line.order_id.warehouse_id.id)
-            res.invoice_id.shop_ids = shop_ids
-        return res
+                if order_line.order_id.warehouse_id.id:
+                    res.invoice_id.shop_id = \
+                        order_line.order_id.warehouse_id.id
+                    return res
