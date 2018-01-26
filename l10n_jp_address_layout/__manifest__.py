34cfd74fedcde0c0f7b3f544395bbcef87d39680
Tim <tl@quartile.co>
2018-01-26 17:44:09 +0800

    [ADD] qrtl-addons (#64)

---
+++ l10n_jp_address_layout/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Japan Address Layout',
+    'version': '11.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.odoo-asia.com',
+    'category': 'Extra Tools',
+    'license': "AGPL-3",
+    'description': """
+This module provides the Japan address input field layout.
+    """,
+    'summary': "",
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+        'data/res_country_data.xml',
+    ],
+    'installable': True,
+}
---
+++ l10n_jp_address_layout_website_sale/models/website.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.http import request
+from odoo import models
+
+
+class Website(models.Model):
+    _inherit = 'website'
+
+    def get_default_country_id(self):
+        country_id = self.env["ir.config_parameter"].sudo().get_param(
+            'l10n_jp_address_layout_website_sale.default_country_id')
+        if not country_id or (country_id and not country_id.isdigit()):
+            country_code = request.session['geoip'].get('country_code')
+            if country_code:
+                res_country = request.env['res.country'].search(
+                    [('code', '=', country_code)], limit=1)
+                if res_country:
+                    country_id = str(res_country.id)
+        return country_id
