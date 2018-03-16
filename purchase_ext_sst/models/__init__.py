12f4dae7e328c667147d4bad248bfc7fe57bc403
tslai <tl@quartile.co>
2018-03-16 08:39:28 +0000

    [FIX] Hook onchange_product_id

--- purchase_ext_sst/models/__init__.py
+++ purchase_ext_sst/models/__init__.py
@@ -3,5 +3,6 @@
 from . import request_channel
 from . import request_medium
 from . import purchase_category
+from . import purchase_order_line_hook_onchange_product_id
 from . import purchase_order
 from . import hr_employee
