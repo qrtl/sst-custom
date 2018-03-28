eaf92009a879ff16b9f5729c173bfe4887ff4ddc
Yoshi Tashiro <tashiro@quartile.co>
2018-03-28 10:37:44 +0000

    [ADD] product_carousel_image_attachment

---
+++ product_carousel_image_attachment/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Product Carousel Image Attachment',
+    'version': '11.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'summary': """""",
+    'description': """
+- When an image (.jpg/.png/.gif/.bmp) is attached to a product, create a
+corresponding product.image record for the attached file.
+    """,
+    'depends': [
+        'product_video_player',
+    ],
+    'installable': True,
+}
---
+++ product_carousel_image_attachment/models/ir_attachment.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models, tools
+
+IMAGE_TYPES = ['image/png', 'image/jpeg', 'image/bmp', 'image/gif']
+
+
+class IrAttachment(models.Model):
+    _inherit = 'ir.attachment'
+
+    @api.model
+    def create(self, vals):
+        #here we resize the image first to avoid bloating the filestore
+        if vals.get('res_model') in ['product.template', 'product.product']:
+            mimetype = vals.get('mimitype') or self._compute_mimetype(vals)
+            if mimetype in IMAGE_TYPES:
+                vals['datas'] = tools.image_resize_image_big(vals['datas'])
+        attachment = super(IrAttachment, self).create(vals)
+        if attachment and attachment.mimetype in IMAGE_TYPES and \
+                        attachment.res_model in ['product.template',
+                                                 'product.product']:
+            #FIXME in case variants are used, how should we normalize the value
+            #assignment for pt and pp below?
+            if attachment.res_model == 'product.template':
+                pt = self.env['product.template'].browse(attachment.res_id)
+                pp = pt.product_variant_id
+            if attachment.res_model == 'product.product':
+                pp = self.env['product.product'].browse(attachment.res_id)
+                pt = pp.product_tmpl_id
+            vals = {
+                'name': attachment.name,
+                'image': attachment.datas,
+                'image_url': attachment.local_url,
+                'product_tmpl_id': pt.id,
+                'product_variant_id': pp.id,
+            }
+            self.env['product.image'].sudo().create(vals)
+        return attachment
