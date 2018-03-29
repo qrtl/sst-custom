e8312550d81283998ec8e7298dd59b40a540a7b6
Tim <tl@quartile.co>
2018-03-29 13:02:19 +0800

    Open product record button in purchase.order.line (#104)

--- purchase_ext_sst/models/__init__.py
+++ purchase_ext_sst/models/__init__.py
@@ -5,4 +5,5 @@ from . import request_medium
 from . import purchase_category
 from . import purchase_order_line_hook_onchange_product_id
 from . import purchase_order
+from . import purchase_order_line
 from . import hr_employee
