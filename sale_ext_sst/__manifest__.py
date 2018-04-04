91ab5eccea270d1d42b0218f97a1926027b6dd67
Tim <tl@quartile.co>
2018-04-04 19:49:01 +0800

    [IMP] remove readonly attribute from requested_date (#109)

--- sale_ext_sst/__manifest__.py
+++ sale_ext_sst/__manifest__.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Quartile Limited
+# Copyright 2017-2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Extension for sales functions',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'category': 'Sale',
     'license': "AGPL-3",
     'description': "",
     'depends': [
         'sale_stock',
+        'sale_order_dates',
     ],
     'data': [
         'views/sale_order_views.xml',
