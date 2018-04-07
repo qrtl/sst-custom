52585e21a8e859ab44ae2c803d1ad196fd69938c
Yoshi Tashiro <tashiro@quartile.co>
2018-04-07 17:43:26 +0000

    [FIX] make invoice_residual a non-stored computed field

--- sale_ext_sst/models/sale_order.py
+++ sale_ext_sst/models/sale_order.py
@@ -15,19 +15,23 @@ class SaleOrder(models.Model):
     requested_date = fields.Datetime(
         readonly=False,
     )
+    #this field is decided to be a non-stored computed field to avoid the
+    #complication of the logic and the risk of missing a trigger.
+    #reconciling/unreconciling invoice with an existing payment particularly
+    #didn't seem to provide the necessary info in context.
     invoice_residual = fields.Monetary(
         'Invoice Amount Due',
         compute='_compute_invoice_residual',
-        store=True,
         help='Remaining amount due of invoice(s).',
     )
 
-    @api.multi
-    @api.depends('invoice_ids.residual')
     def _compute_invoice_residual(self):
-        for order in self:
+        for order in self.filtered(lambda r: r.state in ['sale', 'done']):
             order.invoice_residual = order.amount_total
-            for invoice in order.invoice_ids:
-                if invoice.state not in ('draft', 'cancel'):
-                    order.invoice_residual -= invoice.amount_total \
-                                              - invoice.residual
+            if order.invoice_ids:
+                #we do not cover refund invoices (origin != order.name) here
+                #(it is out of scope)
+                order.invoice_residual -= sum(order.invoice_ids.\
+                    filtered(lambda r: r.origin == order.name and
+                                       r.state in ['open', 'paid']).\
+                    mapped(lambda r: r.amount_total - r.residual))
