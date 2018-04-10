3364f7a4cd9c24818715cd1de2fd94a989910069
Tim <tl@quartile.co>
2018-04-10 14:23:24 +0800

    [FIX] Editable title field in view (#114)

--- product_yahoo_auction_sst/__manifest__.py
+++ product_yahoo_auction_sst/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Products\' Yahoo Auction information',
-    'version': '11.0.1.0.4',
+    'version': '11.0.1.1.0',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'category': 'Product',
