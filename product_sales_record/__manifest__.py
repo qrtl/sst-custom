fdf7727cdbcc95f1b34739b08cf58c3cc66f7d0d
Tim <tl@quartile.co>
2018-04-12 17:33:34 +0800

    [IMP] Update sale price when validating invoice (#116)

--- product_sales_record/__manifest__.py
+++ product_sales_record/__manifest__.py
@@ -3,13 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Product Sales Record',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'category': 'Product',
     'license': "AGPL-3",
     'description': "",
     'depends': [
+        'account',
         'product',
         'sale',
     ],
