990aa6d8b1f14479d6bfd292c50ce57bf0efa979
tslai <tl@quartile.co>
2018-04-13 07:23:11 +0000

    [IMP] Add title to contact widget

--- l10n_jp_address_layout/__manifest__.py
+++ l10n_jp_address_layout/__manifest__.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     'name': 'Japan Address Layout',
-    'version': '11.0.1.0.0',
-    'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
-    'category': 'Extra Tools',
-    'license': "AGPL-3",
-    'description': """
-This module provides the Japan address input field layout.
-    """,
-    'summary': "",
+    'version': '11.0.1.1.0',
     'depends': [
         'base',
     ],
+    'author': 'Quartile Limited, '
+              'Odoo Community Association (OCA)',
+    'license': "LGPL-3",
+    'website': 'https://odoo-community.org/',
+    'category': 'Extra Tools',
+    'description': """
+This module provides Japan address input field layout in views
+The module also extends the default partner title model, allowing the title
+to be shown in most printed reports.
+    """,
     'data': [
+        'views/assets.xml',
+        'views/res_partner_title_views.xml',
         'views/res_partner_views.xml',
+        'views/templates.xml',
         'data/res_country_data.xml',
+        'data/res_partner_title_data.xml',
     ],
     'installable': True,
 }
