17a82406f41cee3a26b49a842f16f3ef5ed9c8d6
Tim <tl@quartile.co>
2018-05-08 16:28:39 +0800

    [IMP] Adjust product.state model (#129)

--- product_ext_sst/__manifest__.py
+++ product_ext_sst/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Extension for product',
-    'version': '11.0.1.3.1',
+    'version': '11.0.2.0.0',
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
     'category': 'Product',
