8b039b9d613f702a2df79741fe819938b5173a38
Tim <tl@quartile.co>
2018-05-08 16:28:39 +0800

    [IMP] Adjust product.state model (#129)

--- product_ext_sst/models/product_state.py
+++ product_ext_sst/models/product_state.py
@@ -2,14 +2,15 @@
 # Copyright 2017-2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProductState(models.Model):
     _name = 'product.state'
     _order = 'sequence, name'
 
-    name = fields.Char(required=True)
+    rank = fields.Char('Rank', required=True)
+    description = fields.Char('Description', required=True)
     sequence = fields.Integer(default=10)
     active = fields.Boolean('Active', default=True)
     company_id = fields.Many2one(
@@ -18,3 +19,10 @@ class ProductState(models.Model):
         default=lambda self: self.env['res.company']._company_default_get(
             'product.state'),
     )
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for record in self:
+            res.append((record.id, record.rank + "：" + record.description))
+        return res
