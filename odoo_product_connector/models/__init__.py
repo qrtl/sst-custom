a83e0f6da9842224b9d5d2412fb3e86a4ad09009
Yoshi Tashiro <tashiro@quartile.co>
2018-07-25 12:49:47 +0000

    [IMP] add connector as dependency

--- odoo_product_connector/models/__init__.py
+++ odoo_product_connector/models/__init__.py
@@ -1,3 +1,5 @@
+from . import backend
+from . import component
 from . import product
 from . import product_sync_history
-from . import res_company
+#from . import res_company
---
+++ odoo_product_connector/models/component.py
@@ -0,0 +1,11 @@
+
+#from odoo.addons.component.core import Component
+
+
+#class NoInheritsBinder(Component):
+#    _name = 'odoo_external.no.inherits.binder'
+#    _inherit = ['base.binder']
+#    _apply_on = [
+#        'odoo_external.binding',
+#    ]
+
