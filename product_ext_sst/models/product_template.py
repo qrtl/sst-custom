7e9ec3a4a9c96bc094960832b71cd16c0ab496d2
Tim <tl@quartile.co>
2018-07-26 19:13:07 +0800

    [IMP] Propose product price during product creation (#163)
    [IMP] Adjust propose logic, add button to manually propose price (#166)
    
    [IMP] Add button to list_view, add public_categ_id (#169)

--- product_ext_sst/models/product_template.py
+++ product_ext_sst/models/product_template.py
@@ -32,6 +32,7 @@ class ProductTemplate(models.Model):
     evaluated_by_id = fields.Many2one('hr.employee', "Evaluated By")
     purchased_by_id = fields.Many2one('hr.employee', "Purchased By")
     shop_id = fields.Many2one('stock.warehouse', string='Shop Purchased')
+    list_price = fields.Float(track_visibility='onchange')
 
     @api.multi
     def open_record(self):
@@ -46,3 +47,52 @@ class ProductTemplate(models.Model):
             'context': {},
             'target': 'current',
         }
+
+    @api.model
+    def create(self, vals):
+        defaults = self.env['product.template'].default_get([
+            'list_price'])
+        if not 'list_price' in vals or vals['list_price'] == \
+                defaults['list_price']:
+            if 'product_state_id' in vals:
+                list_price = self.get_propose_list_price(
+                    vals['name'], vals['product_state_id'])
+            else:
+                list_price = self.get_propose_list_price(vals['name'])
+            if list_price:
+                vals['list_price'] = list_price
+        return super(ProductTemplate, self).create(vals)
+
+    def action_propose_list_price(self):
+        if self.product_state_id:
+            list_price = self.get_propose_list_price(
+                self.name, self.product_state_id.id)
+        else:
+            list_price = self.get_propose_list_price(
+                self.name)
+        if list_price:
+            self.list_price = list_price
+
+    def get_propose_list_price(self, name, state=False):
+        query = """
+            SELECT
+                price
+            FROM
+                product_price_record
+            WHERE
+                %s LIKE '%%' || string || '%%'
+        """
+        query_param = [name]
+        if state:
+            query += """
+            AND
+                product_state_id = %s
+            """
+            query_param.append(state)
+        query += """
+            ORDER BY CHAR_LENGTH(string) DESC
+            LIMIT 1;
+        """
+        self.env.cr.execute(query, query_param)
+        result = self.env.cr.fetchone()
+        return result[0] if result else False
