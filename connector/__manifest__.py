5c60598e6eed6f01e2c09327dee9b86f80d7b782
Tim <tl@quartile.co>
2018-07-30 14:33:12 +0800

    OCA Connector Module (#164)
    * [ADD] OCA connector module
    
    * [ADD] OCA component module
    
    * [ADD] OCA component_event module

---
+++ connector/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013-2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{'name': 'Connector',
+ 'version': '11.0.1.0.0',
+ 'author': 'Camptocamp,Openerp Connector Core Editors,'
+           'Odoo Community Association (OCA)',
+ 'website': 'http://odoo-connector.com',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': ['mail',
+             'queue_job',
+             'component',
+             'component_event',
+             ],
+ 'data': ['security/connector_security.xml',
+          'security/ir.model.access.csv',
+          'views/checkpoint_views.xml',
+          'views/connector_menu.xml',
+          'views/setting_views.xml',
+          'views/res_partner_views.xml',
+          ],
+ 'installable': True,
+ }
