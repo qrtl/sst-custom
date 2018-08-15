74dab2457380f11c05fe793efa670814fa5ccbbf
Tim <tl@quartile.co>
2018-08-15 15:32:52 +0800

    [IMP] Add 'Publish to website' function to stock quant (#170)
    [FIX] Update to yahoo_product_state_id (#172)

--- stock_ext_sst/__manifest__.py
+++ stock_ext_sst/__manifest__.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Quartile Limited
+# Copyright 2017-2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Extension for inventory functions',
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.1.0',
     'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
+    'website': 'https://www.quartile.co',
     'category': 'Stock',
     'license': "AGPL-3",
     'description': "",
     'depends': [
         'stock',
         'delivery',
+        'product_yahoo_auction_sst',
+        'stock_quant_list_view',
     ],
     'data': [
         'views/stock_picking_views.xml',
+        'views/stock_quant_views.xml',
+        'wizard/stock_quant_publish_wizard.xml',
     ],
     'installable': True,
 }
--- stock_ext_sst/models/stock_quant.py
+++ stock_ext_sst/models/stock_quant.py
@@ -8,6 +8,18 @@ from odoo import models, fields, api
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
+    website_published = fields.Boolean(
+        related='product_id.product_tmpl_id.website_published',
+        string='Visible in Website',
+    )
+    yahoo_product_state_id = fields.Many2one(
+        related='product_id.product_tmpl_id.yahoo_product_state_id',
+        string='Yahoo Product State',
+    )
+    list_price = fields.Float(
+        related='product_id.product_tmpl_id.list_price',
+        string='Sale Price',
+    )
     product_id = fields.Many2one(
         auto_join=True,
     )
