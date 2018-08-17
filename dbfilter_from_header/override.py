b0e16bcf913e1e4943d86fd79266ec43e1e0a268
Tim <tl@quartile.co>
2018-08-17 20:00:35 +0800

    [UPD] dbfilter_from_header (#174)

--- dbfilter_from_header/override.py
+++ dbfilter_from_header/override.py
@@ -3,10 +3,13 @@
 # Copyright 2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import logging
 import re
 from odoo import http
+from odoo.tools import config
 
 db_filter_org = http.db_filter
+
 
 def db_filter(dbs, httprequest=None):
     dbs = db_filter_org(dbs, httprequest)
@@ -16,4 +19,9 @@ def db_filter(dbs, httprequest=None):
         dbs = [db for db in dbs if re.match(db_filter_hdr, db)]
     return dbs
 
-http.db_filter = db_filter
+
+if config.get('proxy_mode') and \
+   'dbfilter_from_header' in config.get('server_wide_modules'):
+    _logger = logging.getLogger(__name__)
+    _logger.info('monkey patching http.db_filter')
+    http.db_filter = db_filter
