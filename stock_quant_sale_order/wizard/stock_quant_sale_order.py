899e9526b5323b5786dfc3c270e0147f88512469
Tim <tl@quartile.co>
2018-08-31 14:58:40 +0800

    [FIX] Remove onchange after creation (#178)
    * [FIX] Remove onchange after creation
    
    * [FIX] Revert and assign team_id after onchange

--- stock_quant_sale_order/wizard/stock_quant_sale_order.py
+++ stock_quant_sale_order/wizard/stock_quant_sale_order.py
@@ -77,6 +77,7 @@ class QuantSaleOrderWizard(models.TransientModel):
         }
         sale_order = sale_order_obj.sudo().create(order_vals)
         sale_order.onchange_partner_id()
+        sale_order.team_id = self.team_id.id
         sale_order.user_id = self.env.uid
 
         for quant in quant_ids:
