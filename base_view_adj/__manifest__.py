b6a06293075c76212b7066c3a85214bd2b0c5a3e
Tim <tl@quartile.co>
2018-09-04 17:10:44 +0800

    [IMP] Remove items from the user dropdown (#181)

---
+++ base_view_adj/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Base View Adjustments',
+    'version': '11.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Usability',
+    'license': "AGPL-3",
+    'description': """
+This module removes following items from the user dropdown.
+    - Documentation
+    - Support
+    - My Odoo.com account
+    """,
+    'summary': "",
+    'depends': [
+        'web',
+    ],
+    'qweb': [
+        'static/src/xml/web.xml'
+    ],
+    'installable': True,
+}
