6ec6c7a3cd8708a0309bb947ab782cf6a924853c
Tim <tl@quartile.co>
2018-09-27 18:35:37 +0800

    [IMP] Add popup to warn user before updating the cart (#190)
    [FIX] Adjust translation, logic and template of "Update Cart Content" (#191)

---
+++ website_sale_remove_product_penalty/static/src/js/website_sale.js
@@ -0,0 +1,18 @@
+odoo.define('cart_update_confirm', function (require) {
+    'use strict';
+    require('website.website');
+
+    $(document).ready(function(e){
+        $('.js_delete_product_cart_update').hide();
+        $('.oe_website_spinner_cart_update').hide();
+        $("input.js_quantity").each(function (ev){
+            $(".span_js_product_qty[data-line-id="+$(this).attr("data-line-id")+"]").html($(this).val())
+        });
+        $('.cart_update_confirm').click(function() {
+            $('.js_delete_product_cart_update').show();
+            $('.oe_website_spinner_cart_update').show();
+            $('#cart_update_confirm').hide();
+            $(".span_js_product_qty").hide();
+        });
+    });
+});
