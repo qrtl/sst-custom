3df330d79a4798e018282edc0c8ce14b5d72afb5
tslai <tl@quartile.co>
2018-09-28 05:54:39 +0000

    [FIX] Adjust translation, logic and template

--- website_sale_remove_product_penalty/static/src/js/website_sale.js
+++ website_sale_remove_product_penalty/static/src/js/website_sale.js
@@ -5,6 +5,9 @@ odoo.define('cart_update_confirm', function (require) {
     $(document).ready(function(e){
         $('.js_delete_product_cart_update').hide();
         $('.oe_website_spinner_cart_update').hide();
+        $("input.js_quantity").each(function (ev){
+            $(".span_js_product_qty[data-line-id="+$(this).attr("data-line-id")+"]").html($(this).val())
+        });
         $('.cart_update_confirm').click(function() {
             $('.js_delete_product_cart_update').show();
             $('.oe_website_spinner_cart_update').show();
