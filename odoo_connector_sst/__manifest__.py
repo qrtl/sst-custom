ebeaf8c39d489b435e5a3cb49334d19df92c4313
Yoshi Tashiro <tashiro@quartile.co>
2018-10-22 14:01:32 +0000

    update manifest file

--- odoo_connector_sst/__manifest__.py
+++ odoo_connector_sst/__manifest__.py
@@ -1,3 +1,5 @@
+# © 2013 Guewen Baconnier,Camptocamp SA,Akretion
+# © 2016 Sodexis
 # Copyright 2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
@@ -5,20 +7,11 @@
     'version': '11.0.1.0.0',
     'category' : 'Connector',
     'license': 'AGPL-3',
-    'summary': """This module allow to send product updates to the other Database.""",
+    'summary': """""",
     'description': """
-    - This module allow to send product updates to the another Databse
-
-    - Need to be configured Databse setting
-        - Addded Configuration Setting to the Company Form
-        - Access this setting when run the Job
-
-    - This module allow to send product updates
-        - On the Product form add Sync History to know which fields need to be update
-        - Update Products from the wizard to sync
-        - It's create job at the sync time
-        - Job will be run when other job has been stoped
-
+- This module extends connector module to sync records between the master and
+  satellite databases of Odoo.
+- This module is based on connector_magento module (for ver. 10.0).
     """,
     'author': 'Quartile Limited',
     'website': 'https://www.quartile.co',
@@ -32,7 +25,6 @@
     },
     'data':[
         'security/ir.model.access.csv',
-        # 'wizard/product_sync_view.xml',
         'views/odoo_backend_views.xml',
         'views/connector_odoo_menu.xml',
         'data/connector_odoo_data.xml',
