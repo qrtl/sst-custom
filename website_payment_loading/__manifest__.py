ae63127d686cbe38af3b7e11b93a6e0ee0c2b020
Tim <tl@quartile.co>
2018-11-19 14:24:01 +0800

    [ADD] website_payment_loading (#198)
    [IMP] Add author (#199)
    
    [IMP] Adjust modules' properties, headers and misc (#200)

---
+++ website_payment_loading/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright 2018 Soliton Systems
+# Copyright 2018 Alliance Software Inc.
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Payment Loading Screen',
+    'version': '11.0.1.0.0',
+    'author': 'Soliton Systems,'
+              'Alliance Software Inc.,'
+              'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Payment',
+    'license': "AGPL-3",
+    'description': """
+This module modify the payment page, add a loading screen after user presses
+the "Pay Now" button. This module might not be fully compactible with
+payment acquirers using JS functions to do the checkout, e.g. Stripe.
+
+Attribution: the main parts of the code were extracted from a module
+developed by Soliton and Alliance, and were adjusted by Quartile.
+    """,
+    'depends': [
+        'payment',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+}
---
+++ website_payment_loading/static/src/js/website_payment_loading.js
@@ -0,0 +1,15 @@
+/* Copyright 2018 Soliton Systems
+ * Copyright 2018 Alliance Software Inc.
+ * Copyright 2018 Quartile Limited
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).*/
+
+odoo.define('website_payment_loading.website_payment', function (require) {
+    'use strict';
+    require('website.website');
+    require('web.dom_ready');
+    $('#o_payment_form_pay').on('click',function(ev){
+        if ($(".o_payment_form").find('div.panel-body').find('input[type="radio"]:checked').length != 0){
+            $("#wrap").addClass("loading");
+        }
+    });
+});
