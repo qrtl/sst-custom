bdc6f11c416c013d0f774343014d8e65a6614981
tslai <tl@quartile.co>
2018-11-19 07:22:09 +0000

    [IMP] Add author

--- website_payment_loading/__manifest__.py
+++ website_payment_loading/__manifest__.py
@@ -1,9 +1,11 @@
 # Copyright 2018 Quartile Limited
+# Copyright 2018 Soliton Systems
+# Copyright 2018 Alliance Software Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Website Payment Loading Screen',
     'version': '11.0.1.0.0',
-    'author': 'Quartile Limited',
+    'author': 'Quartile Limited, Soliton Systems, Alliance Software Inc.',
     'website': 'https://www.quartile.co',
     'category': 'Payment',
     'license': "AGPL-3",
