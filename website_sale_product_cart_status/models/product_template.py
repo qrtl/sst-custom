880430645c34c454d4717b24b43c2b6cb9d5ba53
tslai <tl@quartile.co>
2018-12-27 15:54:15 +0800

    [ADD] website_sale_product_cart_status

---
+++ website_sale_product_cart_status/models/product_template.py
@@ -0,0 +1,10 @@
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    in_cart = fields.Boolean(string="In Cart", default=False, readonly=True)
---
+++ website_sale_product_cart_status/models/sale_order_line.py
@@ -0,0 +1,34 @@
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.model
+    def create(self, vals):
+        if 'product_id' in vals:
+            product = self.env['product.product'].browse(vals['product_id'])
+            product.in_cart = True
+        return super(SaleOrderLine, self,).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if 'product_id' in vals:
+            for order_line in self:
+                if order_line.team_id.team_type == 'website':
+                    order_line.product_id.in_cart = False
+        res =  super(SaleOrderLine, self).write(vals)
+        if 'product_id' in vals:
+            for order_line in self:
+                if order_line.team_id.team_type == 'website':
+                    order_line.product_id.in_cart = True
+        return res
+
+    @api.multi
+    def unlink(self):
+        for order_line in self:
+            order_line.product_id.in_cart = False
+        return super(SaleOrderLine, self).unlink()
