3fafe0dc2ef6be6d3429338f06df98330fcae25e
Tim <tl@quartile.co>
2019-03-12 21:41:05 +0800

    [ADD] website_sale_wishlist_archive_cron (#217)

---
+++ website_sale_wishlist_archive_cron/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Sales Wishlist Archive Cron',
+    'version': '11.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Website',
+    'license': "AGPL-3",
+    'description': """
+A cron task that will "Archive" all the wishlist items that the product is 
+inactive
+    """,
+    'depends': [
+        'website_sale_wishlist',
+    ],
+    'data': [
+        'data/cron_data.xml',
+    ],
+    'installable': True,
+}
---
+++ website_sale_wishlist_archive_cron/models/product_wishlist.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api
+
+
+class ProductWishlist(models.Model):
+    _inherit = 'product.wishlist'
+
+    @api.model
+    def _process_wishlist_archive(self):
+        wishlist_records = self.search([('active', '=', True)])
+        for record in wishlist_records:
+            if not record.product_id.active:
+                record.active = True
