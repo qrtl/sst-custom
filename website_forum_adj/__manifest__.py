d5a3dcc5ef02753fac7cd82152a964002a1047b7
Tim <tl@quartile.co>
2019-06-19 13:48:06 +0800

    [ADD] website_forum_adj (#228)

---
+++ website_forum_adj/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Forum Adjustment',
+    'version': '11.0.1.0.0',
+    'author': 'Quartile Limited',
+    'website': 'https://www.quartile.co',
+    'category': 'Website',
+    'license': "AGPL-3",
+    'description': """
+This module removes the social message part after user posts a question/answer
+on website forum.
+    """,
+    'depends': [
+        'website_forum',
+    ],
+    'data': [
+        'views/website_forum.xml'
+    ],
+    'qweb': [
+        'static/src/xml/website_forum_share_templates.xml'
+    ],
+    'installable': True,
+}
