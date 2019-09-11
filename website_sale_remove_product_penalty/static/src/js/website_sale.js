57b624ede649c439106ce3212ea4c7dd82cd8f1e
tslai <tl@quartile.co>
2019-09-11 18:43:39 +0800

    [FIX] Hide cart_update_confirm class

--- website_sale_remove_product_penalty/static/src/js/website_sale.js
+++ website_sale_remove_product_penalty/static/src/js/website_sale.js
@@ -11,7 +11,7 @@ odoo.define('cart_update_confirm', function (require) {
         $('.cart_update_confirm').click(function() {
             $('.js_delete_product_cart_update').show();
             $('.oe_website_spinner_cart_update').show();
-            $('#cart_update_confirm').hide();
+            $('.cart_update_confirm').hide();
             $(".span_js_product_qty").hide();
         });
     });
