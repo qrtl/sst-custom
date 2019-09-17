3ac42f2c252075e6feaddc4ee369dbb3b02f21d2
tslai <tl@quartile.co>
2019-09-17 16:47:40 +0800

    [IMP] Add comments to inherited method

--- website_sale_adj/controllers/main.py
+++ website_sale_adj/controllers/main.py
@@ -8,6 +8,10 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class WebsiteSale(WebsiteSale):
 
+    # Avoid this issue: https://github.com/odoo/odoo/issues/35399
+    # Triggering sale_get_order() in confirmation page so that a new quotation
+    # will be created after the checkout. Hence "Add to Cart" requests will 
+    # not cause the server creating multiple quotations.
     @http.route(['/shop/confirmation'], type='http', auth="public", website=True)
     def payment_confirmation(self, **post):
         res = super(WebsiteSale, self).payment_confirmation(**post)
