7754ac02716da291e1ab9f2ac201c8006460a2ee
Tim <tl@quartile.co>
2019-11-08 19:00:28 +0800

    [IMP] Adjust order confirmation email logic (#247)

--- website_sale_adj/models/sale_order.py
+++ website_sale_adj/models/sale_order.py
@@ -25,7 +25,7 @@ class SaleOrder(models.Model):
             website_order_line = order.website_order_line
             order.order_line_date = website_order_line[0].create_date if website_order_line else False
 
-    # Since quotation will be created after checked, use order_line_date to 
+    # Since quotation will be created after checked, use order_line_date to
     # compute is_abandoned_cart field instead of date_order
     @api.multi
     @api.depends('team_id.team_type', 'order_line_date', 'order_line', 'state', 'partner_id')
@@ -55,3 +55,18 @@ class SaleOrder(models.Model):
         if (operator not in expression.NEGATIVE_TERM_OPERATORS and value) or (operator in expression.NEGATIVE_TERM_OPERATORS and not value):
             return abandoned_domain
         return expression.distribute_not(abandoned_domain)  # negative domain
+
+    # Overwrite force_quotation_send and put the email in queue instead of sending it immediately
+    @api.multi
+    def force_quotation_send(self):
+        for order in self:
+            email_act = order.action_quotation_send()
+            if email_act and email_act.get('context'):
+                email_ctx = email_act['context']
+                email_ctx.update(default_email_from=order.company_id.email)
+                # order.with_context(email_ctx).message_post_with_template(email_ctx.get('default_template_id'))
+                self.env['mail.template'].browse(email_ctx.get(
+                    'default_template_id')).with_context(email_ctx).send_mail(order.id)
+                if 'mark_so_as_sent' in email_ctx and email_ctx['mark_so_as_sent'] and order.state == 'draft':
+                    order.with_context(tracking_disable=True).state = 'sent'
+        return True
