97843839d8fac90b269c8983897dbc9f8312bf60
Sokream Phan <sokream@quartile.co>
2019-11-08 15:06:16 +0800

    remove one constrain from setting

--- website_sale_product_subscribe_notify/models/res_config_settings.py
+++ website_sale_product_subscribe_notify/models/res_config_settings.py
@@ -26,6 +26,6 @@ class ResConfigSettings(models.TransientModel):
 
     @api.constrains('email_recipient_limit')
     def _check_email_recipient_limit(self):
-        if (self.email_recipient_limit <= 0 or self.email_recipient_limit > 100):
+        if (self.email_recipient_limit <= 0):
             raise ValidationError("Limit Email Recipient needs to be greater than 0 and less than 100.")
             
