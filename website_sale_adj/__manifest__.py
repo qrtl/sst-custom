a6152d89181b381c0134cc51981400fd09724445
tslai <tl@quartile.co>
2019-12-31 16:10:53 +0800

    [IMP] Add followers to bcc, update reply_to

--- website_sale_adj/__manifest__.py
+++ website_sale_adj/__manifest__.py
@@ -14,10 +14,12 @@ This module modify the website_sale module and provide following feature(s):
  - Hide the "Shop" menu in header from public user.
     """,
     'depends': [
-        'website_sale',
+        'mail_bcc',
         'product_ext_sst',
+        'website_sale',
     ],
     'data': [
+        'data/mail_template_data.xml',
         'views/res_config_settings_views.xml',
         'views/templates.xml',
         'security/website_sale_security.xml',
