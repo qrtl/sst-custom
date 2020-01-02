db34f9b176396798e7323bffe984496e3caf7d39
tslai <tl@quartile.co>
2020-01-02 14:37:19 +0800

    [FIX] Only update draft website orders

--- website_sale_commission/models/sale_order_line.py
+++ website_sale_commission/models/sale_order_line.py
@@ -12,9 +12,11 @@ class SaleOrderLine(models.Model):
     @api.multi
     def write(self, vals):
         res = super(SaleOrderLine, self).write(vals)
-        if 'product_uom_qty' in vals or 'price_unit' in vals:
+        if 'product_uom_qty' in vals or 'price_unit' in vals or 'tax_id' in \
+                vals or 'discount' in vals:
             for order_line in self:
-                if order_line.product_id.type != 'service' and \
+                if order_line.order_id.state == 'draft' and \
+                    order_line.product_id.type != 'service' and \
                         order_line.order_id.team_id and \
                         order_line.order_id.team_id.team_type == 'website':
                     order_line.order_id.update_website_sale_commission()
@@ -24,7 +26,8 @@ class SaleOrderLine(models.Model):
     def create(self, vals):
         res = super(SaleOrderLine, self).create(vals)
         for order_line in res:
-            if order_line.product_id.type != 'service' and \
+            if order_line.order_id.state == 'draft' and \
+                order_line.product_id.type != 'service' and \
                     order_line.order_id.team_id and \
                     order_line.order_id.team_id.team_type == 'website':
                 order_line.order_id.update_website_sale_commission()
@@ -36,7 +39,8 @@ class SaleOrderLine(models.Model):
         update_flag = self.filtered(lambda l: not l.is_commission)
         res = super(SaleOrderLine, self).unlink()
         if update_flag:
-            for order in orders.filtered(lambda o: o.team_id and
-                                         o.team_id.team_type == 'website'):
+            for order in orders.filtered(
+                lambda o: o.team_id and o.state == 'draft' and
+                    o.team_id.team_type == 'website'):
                 order.update_website_sale_commission()
         return res
