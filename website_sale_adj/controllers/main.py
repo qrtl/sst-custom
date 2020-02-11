0055fa8d283162a9d9de8cbc3c698fa73ef8897d
Tim <tl@quartile.co>
2020-02-11 18:36:45 +0800

    [IMP] Make write_date as the website product order (#257)
    * [IMP] Make write_date as the website product order
    
    * [IMP] Add comments

--- website_sale_adj/controllers/main.py
+++ website_sale_adj/controllers/main.py
@@ -10,10 +10,18 @@ class WebsiteSale(WebsiteSale):
 
     # Avoid this issue: https://github.com/odoo/odoo/issues/35399
     # Triggering sale_get_order() in confirmation page so that a new quotation
-    # will be created after the checkout. Hence "Add to Cart" requests will 
+    # will be created after the checkout. Hence "Add to Cart" requests will
     # not cause the server creating multiple quotations.
     @http.route(['/shop/confirmation'], type='http', auth="public", website=True)
     def payment_confirmation(self, **post):
         res = super(WebsiteSale, self).payment_confirmation(**post)
         request.website.sale_get_order(force_create=True)
         return res
+
+    # Overwrite _get_search_order() so the product will be sorted by
+    # write_date in the website
+    def _get_search_order(self, post):
+        # OrderBy will be parsed in orm and so no direct sql injection
+        # id is added to be sure that order is a unique sort key
+        # return 'website_published desc,%s , id desc' % post.get('order', 'website_sequence desc')
+        return 'website_published desc, write_date desc, id desc'
