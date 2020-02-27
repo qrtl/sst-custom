dd714ba875430da84e98fccf58a5d5c89a1b94fb
Tim <tl@quartile.co>
2020-02-27 18:05:56 +0800

    [IMP] Add website_sequence_date for ordering (#259)
    * [IMP] Add website_sequence_date for ordering
    
    * [IMP] Add translations

--- website_sale_adj/controllers/main.py
+++ website_sale_adj/controllers/main.py
@@ -19,9 +19,9 @@ class WebsiteSale(WebsiteSale):
         return res
 
     # Overwrite _get_search_order() so the product will be sorted by
-    # write_date in the website
+    # website_sequence_date in the website
     def _get_search_order(self, post):
         # OrderBy will be parsed in orm and so no direct sql injection
         # id is added to be sure that order is a unique sort key
         # return 'website_published desc,%s , id desc' % post.get('order', 'website_sequence desc')
-        return 'website_published desc, write_date desc, id desc'
+        return 'website_published desc, website_sequence_date desc, id desc'
