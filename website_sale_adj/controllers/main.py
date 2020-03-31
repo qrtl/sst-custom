5612b1f9e2b9b78c9f000629a16e12ddb20822cd
Tim <tl@quartile.co>
2020-03-31 17:35:19 +0800

    [IMP] Remove order create logic when checkout (#265)

--- website_sale_adj/controllers/main.py
+++ website_sale_adj/controllers/main.py
@@ -1,22 +1,10 @@
 # Copyright 2019 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import http
-from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class WebsiteSale(WebsiteSale):
-
-    # Avoid this issue: https://github.com/odoo/odoo/issues/35399
-    # Triggering sale_get_order() in confirmation page so that a new quotation
-    # will be created after the checkout. Hence "Add to Cart" requests will
-    # not cause the server creating multiple quotations.
-    @http.route(['/shop/confirmation'], type='http', auth="public", website=True)
-    def payment_confirmation(self, **post):
-        res = super(WebsiteSale, self).payment_confirmation(**post)
-        request.website.sale_get_order(force_create=True)
-        return res
 
     # Overwrite _get_search_order() so the product will be sorted by
     # website_sequence_date in the website
