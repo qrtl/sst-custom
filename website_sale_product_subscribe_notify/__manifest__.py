696d20959dcb13e5a966b4b211abaaf60f0cfa19
tslai <tl@quartile.co>
2020-04-14 16:26:11 +0800

    [IMP] Bump version

--- website_sale_product_subscribe_notify/__manifest__.py
+++ website_sale_product_subscribe_notify/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Odoo Product Subscribe Notify',
-    'version': '11.0.2.0.0',
+    'version': '11.0.2.0.1',
     'category': 'Website',
     'license': 'Other proprietary',
     'summary': "Send notification to subscribers",
