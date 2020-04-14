3e0577ebb2a4c8af23785afdb957726fd5919ab8
tslai <tl@quartile.co>
2020-04-14 18:04:27 +0800

    [ADD] product_shelf_management

---
+++ product_shelf_management/models/product_template.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    shelf_id = fields.Char(
+        string="Shelf ID",
+        default=False,
+        copy=False
+    )
