1fd82a3343df82723414194aaf0ca269157b8d62
tslai <tl@quartile.co>
2020-04-15 17:22:09 +0800

    [IMP] Rename module, adjust delivery ship report

--- product_shelf_info/__manifest__.py
+++ product_shelf_info/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Product Shelf Management",
+    "name": "Product Shelf Information",
     "version": "11.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
