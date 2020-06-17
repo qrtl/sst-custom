9fe4ac214c040c04982f52e968877fd4ee07867b
Tim <tl@quartile.co>
2020-06-17 10:25:03 +0800

    [1063][ADD] account_invoice_sale_order_warehouse (#273)

---
+++ account_invoice_warehouse/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Account Invoice's Warehouse",
+    "version": "11.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Accounting",
+    "license": "LGPL-3",
+    "depends": ["sale_stock"],
+    "data": ["views/account_invoice_views.xml", "views/templates.xml"],
+    "installable": True,
+}
---
+++ account_invoice_warehouse/models/account_invoice.py
@@ -0,0 +1,22 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    warehouse_id = fields.Many2one(
+        "stock.warehouse",
+        string="Sale Order Warehouse",
+        compute="_compute_warehouse_id",
+        help="Store the warehouse_id of the sale order.",
+    )
+
+    @api.multi
+    def _compute_warehouse_id(self):
+        for invoice in self:
+            order = invoice.invoice_line_ids.mapped("sale_line_ids").mapped("order_id")
+            if order:
+                invoice.warehouse_id = order[0].warehouse_id
