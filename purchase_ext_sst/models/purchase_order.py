b427ec361ebb410d3902a212a908247fc45cc4b5
Tim <tl@quartile.co>
2020-07-16 13:42:26 +0800

    [IMP] Add picking_type_id to purchase tree view (#277)

--- purchase_ext_sst/models/purchase_order.py
+++ purchase_ext_sst/models/purchase_order.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017-2018 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
--- purchase_ext_sst/models/purchase_order_line_hook_onchange_product_id.py
+++ purchase_ext_sst/models/purchase_order_line_hook_onchange_product_id.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from datetime import datetime
 
