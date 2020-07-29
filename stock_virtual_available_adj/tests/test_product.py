c80ea909521a55a34638bfb18bc1cad2096e7a0a
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-07-29 15:59:56 +0530

    [FIX]Rebase branch and black flake-8

--- stock_virtual_available_adj/tests/test_product.py
+++ stock_virtual_available_adj/tests/test_product.py
@@ -8,36 +8,33 @@ class TestProduct(common.TransactionCase):
     """
         This tests allows to check website sale available quantity for product.
     """
+
     def setUp(self):
         super(TestProduct, self).setUp()
 
-        self.sale_order_line = self.env['sale.order.line']
-        self.stock_location = self.env.ref('stock.stock_location_stock')
-        self.product_uom = self.env.ref('product.product_uom_unit').id
+        self.sale_order_line = self.env["sale.order.line"]
+        self.stock_location = self.env.ref("stock.stock_location_stock")
+        self.product_uom = self.env.ref("product.product_uom_unit").id
 
         # Create Test Products
-        self.product_01 = self.env['product.product'].create({
-            'name': "Test Product 01",
-            'type': 'product',
-        })
-        self.product_02 = self.env['product.product'].create({
-            'name': "Test Product 02",
-            'type': 'product',
-        })
+        self.product_01 = self.env["product.product"].create(
+            {"name": "Test Product 01", "type": "product"}
+        )
+        self.product_02 = self.env["product.product"].create(
+            {"name": "Test Product 02", "type": "product"}
+        )
 
         # update the quantity on hand for products
-        self.env['stock.quant']._update_available_quantity(
-            self.product_01, self.stock_location, 100)
-        self.env['stock.quant']._update_available_quantity(
-            self.product_02, self.stock_location, 100)
+        self.env["stock.quant"]._update_available_quantity(
+            self.product_01, self.stock_location, 100
+        )
+        self.env["stock.quant"]._update_available_quantity(
+            self.product_02, self.stock_location, 100
+        )
 
         # Create Test Partners
-        self.partner_01 = self.env['res.partner'].create({
-            'name': 'Test Partner 1',
-        })
-        self.partner_02 = self.env['res.partner'].create({
-            'name': 'Test Partner 2',
-        })
+        self.partner_01 = self.env["res.partner"].create({"name": "Test Partner 1"})
+        self.partner_02 = self.env["res.partner"].create({"name": "Test Partner 2"})
 
     def test_compute_website_sale_available_qty_01(self):
         """
@@ -50,87 +47,101 @@ class TestProduct(common.TransactionCase):
         # `sent` state and compute their quantity.
 
         # I create a sales order
-        self.sale_order = self.env['sale.order'].create({
-            'partner_id': self.partner_01.id,
-        })
+        self.sale_order = self.env["sale.order"].create(
+            {"partner_id": self.partner_01.id}
+        )
 
         # In the sales order I add some sales order lines.
-        self.sale_order_line.create({
-            'product_id': self.product_01.id,
-            'price_unit': 100.00,
-            'product_uom': self.product_uom,
-            'product_uom_qty': 20.0,
-            'order_id': self.sale_order.id,
-            'state': 'sent'
-        })
-        self.sale_order_line.create({
-            'product_id': self.product_02.id,
-            'price_unit': 100.00,
-            'product_uom': self.product_uom,
-            'product_uom_qty': 30.0,
-            'order_id': self.sale_order.id,
-            'state': 'sent'
-        })
+        self.sale_order_line.create(
+            {
+                "product_id": self.product_01.id,
+                "price_unit": 100.00,
+                "product_uom": self.product_uom,
+                "product_uom_qty": 20.0,
+                "order_id": self.sale_order.id,
+                "state": "sent",
+            }
+        )
+        self.sale_order_line.create(
+            {
+                "product_id": self.product_02.id,
+                "price_unit": 100.00,
+                "product_uom": self.product_uom,
+                "product_uom_qty": 30.0,
+                "order_id": self.sale_order.id,
+                "state": "sent",
+            }
+        )
 
-        self.sale_order = self.env['sale.order'].create({
-            'partner_id': self.partner_02.id,
-        })
+        self.sale_order = self.env["sale.order"].create(
+            {"partner_id": self.partner_02.id}
+        )
 
         # In the sales order I add some sales order lines.
-        self.sale_order_line.create({
-            'product_id': self.product_01.id,
-            'price_unit': 100.00,
-            'product_uom': self.product_uom,
-            'product_uom_qty': 10.0,
-            'order_id': self.sale_order.id,
-            'state': 'sent'
-        })
+        self.sale_order_line.create(
+            {
+                "product_id": self.product_01.id,
+                "price_unit": 100.00,
+                "product_uom": self.product_uom,
+                "product_uom_qty": 10.0,
+                "order_id": self.sale_order.id,
+                "state": "sent",
+            }
+        )
 
-        self.sale_order_line.create({
-            'product_id': self.product_02.id,
-            'price_unit': 100.00,
-            'product_uom': self.product_uom,
-            'product_uom_qty': 25.0,
-            'order_id': self.sale_order.id,
-            'state': 'sent'
-        })
+        self.sale_order_line.create(
+            {
+                "product_id": self.product_02.id,
+                "price_unit": 100.00,
+                "product_uom": self.product_uom,
+                "product_uom_qty": 25.0,
+                "order_id": self.sale_order.id,
+                "state": "sent",
+            }
+        )
 
-        # Call the `_compute_website_sale_available_qty` method for product 01(product.product)
+        # Call the `_compute_website_sale_available_qty`
+        # method for product 01(product.product)
         self.product_01._compute_website_sale_available_qty()
 
         # Compare the `website_sale_available_qty` qty for product 01(product.product)
         self.assertEqual(
             self.product_01.website_sale_available_qty,
             70,
-            'Check website sale available qty'
+            "Check website sale available qty",
         )
 
-        # Call the `_compute_website_sale_available_qty` method for product 02(product.product)
+        # Call the `_compute_website_sale_available_qty`
+        # method for product 02(product.product)
         self.product_02._compute_website_sale_available_qty()
 
-        # Compare the `website_sale_available_qty` qty for product 02(product.product)
+        # Compare the `website_sale_available_qty`
+        # qty for product 02(product.product)
         self.assertEqual(
             self.product_02.website_sale_available_qty,
             45,
-            'Check website sale available qty'
+            "Check website sale available qty",
         )
 
-        # Call the `_compute_website_sale_available_qty` method for product 01(product.template)
+        # Call the `_compute_website_sale_available_qty`
+        # method for product 01(product.template)
         self.product_01.product_tmpl_id._compute_website_sale_available_qty()
 
-        # Compare the `website_sale_available_qty` qty for product 01(product.template)
+        # Compare the `website_sale_available_qty`
+        # qty for product 01(product.template)
         self.assertEqual(
             self.product_01.product_tmpl_id.website_sale_available_qty,
             140,
-            'Check website sale available qty'
+            "Check website sale available qty",
         )
 
-        # Call the `_compute_website_sale_available_qty` method for product 02(product.template)
+        # Call the `_compute_website_sale_available_qty`
+        # method for product 02(product.template)
         self.product_02.product_tmpl_id._compute_website_sale_available_qty()
 
         # Compare the `website_sale_available_qty` qty for product 02(product.template)
         self.assertEqual(
             self.product_02.product_tmpl_id.website_sale_available_qty,
             90,
-            'Check website sale available qty'
+            "Check website sale available qty",
         )
