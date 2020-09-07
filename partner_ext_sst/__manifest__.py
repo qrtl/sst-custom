7b05601e79e2e9b06610822b67225e869919f67b
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-09-07 06:36:27 +0530

    [1224] Update Production for Reformated code. (#286)

--- partner_ext_sst/__manifest__.py
+++ partner_ext_sst/__manifest__.py
@@ -1,22 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Extension for partner',
-    'version': '11.0.1.1.0',
-    'author': 'Quartile Limited',
-    'website': 'https://www.odoo-asia.com',
-    'category': 'Purchase',
-    'license': "AGPL-3",
-    'description': "",
-    'depends': [
-        'purchase',
+    "name": "Extension for partner",
+    "version": "11.0.1.1.0",
+    "author": "Quartile Limited",
+    "website": "https://www.odoo-asia.com",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_identification_type_views.xml",
+        "views/res_occupation_views.xml",
+        "views/res_partner_views.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/res_identification_type_views.xml',
-        'views/res_occupation_views.xml',
-        'views/res_partner_views.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
--- partner_ext_sst/models/res_identification_type.py
+++ partner_ext_sst/models/res_identification_type.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResIdentificationType(models.Model):
-    _name = 'res.identification.type'
+    _name = "res.identification.type"
 
-    name = fields.Char(
-        required=True
-    )
+    name = fields.Char(required=True)
