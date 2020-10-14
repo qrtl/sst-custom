d82f4c4d9a24f18dca3e5730fb955b21e3fe1a7b
tslai <tl@quartile.co>
2020-10-14 14:20:05 +0800

    [896][IMP] Add mail_outbound_static as dependency

--- website_forum_adj/__manifest__.py
+++ website_forum_adj/__manifest__.py
@@ -1,16 +1,15 @@
-# Copyright 2019 Quartile Limited
+# Copyright 2019-2020 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Website Forum Adjustment",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Website",
     "license": "AGPL-3",
-    "depends": ["website_forum"],
+    "depends": ["website_forum", "mail_outbound_static"],
     "data": [
         "data/mail_data.xml",
-        "views/ir_mail_server_views.xml",
         "views/res_config_settings_views.xml",
         "views/website_forum.xml",
     ],
