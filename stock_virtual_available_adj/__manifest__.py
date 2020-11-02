40e1bc41990883f0b3a87f9e61da51f168f97bda
Ryoko <ryoko@quartile.co>
2020-11-02 09:05:34 +0800

    [IMP]change-color-negative-website_sale_available_qty

--- stock_virtual_available_adj/__manifest__.py
+++ stock_virtual_available_adj/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Virtual Available Adjust",
-    "version": "11.0.1.2.1",
+    "version": "11.0.1.2.2",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Warehouse",
