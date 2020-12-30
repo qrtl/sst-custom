2da9d856aa58b086023ed72f18063f643af2121d
tslai <tl@quartile.co>
2020-12-30 17:59:06 +0800

    [IMP] Hide street2 field from list view

--- purchase_post_code_propose_address_sst/__manifest__.py
+++ purchase_post_code_propose_address_sst/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2019 Quartile Limited
+# Copyright 2019-2020 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Post Code (JP) Propose Purchase Order Address",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Purchase",
