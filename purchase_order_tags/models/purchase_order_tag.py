e94ddcd8cdab3a7608d0a33c0f41d6c671490d29
tslai <tl@quartile.co>
2020-12-30 17:58:29 +0800

    [ADD] purchase_order_tags

---
+++ purchase_order_tags/models/purchase_order_tag.py
@@ -0,0 +1,10 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class PurchaseOrderTag(models.Model):
+    _name = 'purchase.order.tag'
+
+    name = fields.Char(required=True, translate=True)
