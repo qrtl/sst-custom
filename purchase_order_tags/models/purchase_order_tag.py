3eca8d416a7ab71766f0b47d8526e3d15037ac03
tslai <tl@quartile.co>
2020-12-31 20:57:39 +0800

    [ADD] Black, isort

--- purchase_order_tags/models/purchase_order_tag.py
+++ purchase_order_tags/models/purchase_order_tag.py
@@ -8,5 +8,8 @@ class PurchaseOrderTag(models.Model):
     _name = "purchase.order.tag"
 
     name = fields.Char(required=True, translate=True)
-    color = fields.Integer(string='Color Index')
-    active = fields.Boolean(default=True, help="The active field allows you to hide the tag without removing it.")
+    color = fields.Integer(string="Color Index")
+    active = fields.Boolean(
+        default=True,
+        help="The active field allows you to hide the tag without removing it.",
+    )
