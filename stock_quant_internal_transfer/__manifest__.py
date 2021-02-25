dcce8c31f3e9c1962c6ccf10e39bd5fa726db035
tslai <tl@quartile.co>
2021-02-25 10:40:15 +0800

    [IMP] Update readme

--- stock_quant_internal_transfer/__manifest__.py
+++ stock_quant_internal_transfer/__manifest__.py
@@ -1,12 +1,12 @@
-# Copyright 2017 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017-2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Stock Quant Internal Transfer",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "author": "Quartile Limited",
     "website": "https://www.odoo-asia.com",
     "category": "Warehouse",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "summary": """Make internal transfer from quant""",
     "depends": ["stock"],
     "data": ["wizard/stock_quant_transfer_wizard.xml"],
