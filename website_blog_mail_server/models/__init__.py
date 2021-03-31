14e8e01a36269acf8d8b3942757ed84156d431fe
tslai <tl@quartile.co>
2021-03-31 15:15:33 +0800

    [REM] website_blog_mail_server

--- website_blog_mail_server/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import mail_message
-from . import res_config_settings
-from . import res_partner
-from . import website
--- website_blog_mail_server/readme/DESCRIPTION.rst
+++
@@ -1,3 +0,0 @@
-This module does the following:
-
-* Sends blog update notification emails from a specific email server.
