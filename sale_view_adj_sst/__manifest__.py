72b0a9dd2429e95593c2ae277519dfd53a0cb1c5
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-06-15 11:54:59 +0800

    Add Module template & quote from CookBook

---
+++ sale_view_adj_sst/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sale View Adjuster",
+    "category": "Accounting",
+    "version": "11.0.1.1.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co/",
+    "license": "LGPL-3",
+    "depends": ["sale_management"],
+    "data": ["views/sale_view_adj.xml"],
+    "installable": True,
+}
