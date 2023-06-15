e5db0754ea5c39e249c09c3e4ae8f2ca2347953c
Tatsuki Kanda (Quratile) <101688144+kanda999@users.noreply.github.com>
2023-06-15 17:34:48 +0900

    [3478][ADD] account_partial_reconcile_restrict #332
    [3478][ADD] account_partial_reconcile_restrict

---
+++ account_partial_reconcile_restrict/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Partial Reconcile Restrict",
+    "version": "15.0.1.0.0",
+    "category": "Invoices & Payments",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["account"],
+    "license": "AGPL-3",
+    "data": [],
+    "installable": True,
+}
