6c1c44d384f96cd318fdcba38f11182dcbb35d0e
kanda999 <kanda@quartile.co>
2023-06-28 17:59:25 +0800

    upd

--- purchase_order_category/__manifest__.py
+++ purchase_order_category/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Order Category",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Purchase",
---
+++ purchase_order_category/migrations/11.0.1.0.1/post-migration.py
@@ -0,0 +1,20 @@
+# # Copyright 2023 Quartile Limited
+
+# from openupgradelib import openupgrade
+
+# MODULE_LIST = ["purchase_ext_sst"]
+# fields_list = ["purchase_ext_sst_purchase_category_id"]
+
+# @openupgrade.migrate()
+# def migrate(env, version):
+#     # Update the module reference in external identifiers
+#     openupgrade.logged_query(
+#         env.cr,
+#         """
+#         UPDATE ir_model_data
+#         SET module = 'purchase_order_category'
+#         WHERE module IN %s AND model = 'ir.model.fields' and name in %s;
+#         """,
+#         (tuple(MODULE_LIST),(tuple(fields_list),))
+#     )
+
