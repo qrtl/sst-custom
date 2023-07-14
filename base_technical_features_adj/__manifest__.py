52ae981c5d75e90ad493ebd1f809a27c80638ce5
Aungkokolin1997 <lin@quartile.co>
2023-07-14 10:10:12 +0630

    [MIG] base_technical_features_adj: Migration to 15.0

--- base_technical_features_adj/__manifest__.py
+++ base_technical_features_adj/__manifest__.py
@@ -2,14 +2,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Technical features group adjustments",
-    "version": "11.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Usability",
     "license": "AGPL-3",
-    "summary": """
-This module hides the "Technical Features" option in the Preferences form view.
-    """,
     "depends": ["base_technical_features"],
     "data": ["views/res_users_views.xml"],
     "installable": True,
