467a9e4c1c182489fca34d5700fb3a302a197d81
Aungkokolin1997 <aungkokolin23397@gmail.com>
2023-07-21 10:54:17 +0800

    update

--- product_delivery_destination/models/product_template.py
+++ product_delivery_destination/models/product_template.py
@@ -8,16 +8,16 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     delivery_state_id = fields.Many2one('res.country.state', string="Delivery Prefecture", domain="[('is_deliverable','=', True)]")
-    delivery_city_id = fields.Many2one('delivery.city', string="Delivery City")
-
+    delivery_city_id = fields.Many2one('delivery.city', string="Delivery City", domain="[('id', 'in', selectable_delivery_city_ids)]",)
+    selectable_delivery_city_ids = fields.Many2many(
+        comodel_name="delivery.city", compute="_compute_delivery_city_ids"
+    )
     @api.onchange("delivery_city_id")
     def _onchange_delivery_city_id(self):
         if self.delivery_city_id:
             self.delivery_state_id = self.delivery_city_id.state_id
     
-    @api.onchange("delivery_state_id")
-    def _onchange_delivery_state_id(self):
-        if self.delivery_state_id:
-            return {'domain': {'delivery_city_id': [('state_id', '=', self.delivery_state_id.id)]}}
-        else:
-            return {'domain': {'delivery_city_id': []}}
+    @api.depends("delivery_state_id")
+    def _compute_delivery_city_ids(self):
+        for rec in self:
+            rec.selectable_delivery_city_ids = self.env["delivery.city"].search([('state_id', '=', rec.delivery_state_id.id)])
