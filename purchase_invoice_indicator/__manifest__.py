a296e3b6f85407d887362b204bc703c6be4dba48
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-07-25 08:47:16 +0630

    [3537][ADD] purchase_invoice_indicator (#340)
    * [ADD] partner_invoice_target

---
+++ purchase_invoice_indicator/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Invoice Indicator",
+    "version": "11.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/account_invoice_views.xml"
+    ],
+    "installable": True,
+}
---
+++ purchase_invoice_indicator/models/res_partner.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models,fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    is_invoice_issuer = fields.Boolean("Qualified Invoice Issuer")
