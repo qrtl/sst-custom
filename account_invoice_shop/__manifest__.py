c4c132eeb617c5b1303adfb9189bfeb8eb3a8c41
kanda999 <kanda@quartile.co>
2023-08-15 17:46:52 +0800

    add mig script

--- account_invoice_shop/__manifest__.py
+++ account_invoice_shop/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Invoice Shop",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Accounting",
