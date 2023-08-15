08f065f6e4fd20d1465e74d6d9258c77dd5609b7
kanda999 <kanda@quartile.co>
2023-08-15 18:07:39 +0800

    Adjust view of account_invoice_shop

--- account_invoice_shop/11.0.1.0.1/post-migration.py
+++ account_invoice_shop/11.0.1.0.1/post-migration.py
@@ -6,19 +6,19 @@ MODULE_LIST = ["account_ext_sst"]
 fields_list = ["shop_id"]
 
 
-# @openupgrade.migrate()
-# def migrate(env, version):
-#     # Update the module reference in external identifiers
-#     for field in fields_list:
-#         openupgrade.logged_query(
-#             env.cr,
-#             """
-#             UPDATE ir_model_data
-#             SET module = 'account_invoice_shop'
-#             WHERE module IN %s AND model = 'ir.model.fields' and name LIKE %s;
-#             """,
-#             (
-#                 tuple(MODULE_LIST),
-#                 "%" + field + "%",
-#             ),
-#         )
+@openupgrade.migrate()
+def migrate(env, version):
+    # Update the module reference in external identifiers
+    for field in fields_list:
+        openupgrade.logged_query(
+            env.cr,
+            """
+            UPDATE ir_model_data
+            SET module = 'account_invoice_shop'
+            WHERE module IN %s AND model = 'ir.model.fields' and name LIKE %s;
+            """,
+            (
+                tuple(MODULE_LIST),
+                "%" + field + "%",
+            ),
+        )
