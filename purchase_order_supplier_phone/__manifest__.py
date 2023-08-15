30405e949ac051a0286c2e9a7f0a1e3543ae2211
kanda999 <kanda@quartile.co>
2023-08-15 16:28:54 +0800

    translation fix manifest

--- purchase_order_supplier_phone/__manifest__.py
+++ purchase_order_supplier_phone/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2017 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Purchase Supplier Info",
+    "name": "Purchase Order Supplier Phone",
     "version": "11.0.1.0.1",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
