d2484f1d030d0e78794e625dfb10762ff1a321aa
kanda999 <kanda@quartile.co>
2023-09-05 11:43:49 +0800

    upd readme code style

--- purchase_order_round_adj/models/purchase_order.py
+++ purchase_order_round_adj/models/purchase_order.py
@@ -1,26 +1,31 @@
 # Copyright 2023 Quartile Limited
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import api, models
 
 
 class PurcahseOrder(models.Model):
     _inherit = "purchase.order"
 
-    @api.depends('order_line.price_total')
+    @api.depends("order_line.price_total")
     def _amount_all(self):
-        # This method does not work for line items that include both inclusive and exclusive taxes. 
-        # It assumes that the sales order only contains lines with either inclusive or exclusive taxes.
-        res = super()._amount_all()
+        # This method does not work for line items that include
+        # both inclusive and exclusive taxes.
+        # It assumes that the sales order only contains lines
+        # with either inclusive or exclusive taxes.
+        super()._amount_all()
         for order in self:
             amount_tax = amount_total = 0.0
             for line in order.order_line:
-                # If exclusive tax is included in the sale.order.line, the original logic will be used.
+                # If exclusive tax is included in the sale.order.line,
+                # the original logic will be used.
                 if any(not tax.price_include for tax in line.taxes_id):
                     return
                 amount_tax += line.price_tax
                 amount_total += line.price_total
-            order.update({
-                'amount_untaxed': amount_total - amount_tax, 
-                'amount_total': amount_total,
-            })
+            order.update(
+                {
+                    "amount_untaxed": amount_total - amount_tax,
+                    "amount_total": amount_total,
+                }
+            )
--- sale_order_round_adj/models/sale_order.py
+++ sale_order_round_adj/models/sale_order.py
@@ -1,26 +1,31 @@
 # Copyright 2023 Quartile Limited
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.depends('order_line.price_total')
+    @api.depends("order_line.price_total")
     def _amount_all(self):
-        # This method does not work for line items that include both inclusive and exclusive taxes. 
-        # It assumes that the sales order only contains lines with either inclusive or exclusive taxes.
-        res = super()._amount_all()
+        # This method does not work for line items
+        # that include both inclusive and exclusive taxes.
+        # It assumes that the sales order only contains lines
+        # with either inclusive or exclusive taxes.
+        super()._amount_all()
         for order in self:
             amount_tax = amount_total = 0.0
             for line in order.order_line:
-                # If exclusive tax is included in the sale.order.line, the original logic will be used.
+                # If exclusive tax is included in the sale.order.line,
+                # the original logic will be used.
                 if not line.tax_id.price_include:
                     return
                 amount_tax += line.price_tax
                 amount_total += line.price_total
-            order.update({
-                'amount_untaxed': amount_total - amount_tax, 
-                'amount_total': amount_total,
-            })
+            order.update(
+                {
+                    "amount_untaxed": amount_total - amount_tax,
+                    "amount_total": amount_total,
+                }
+            )
