709da6e363e7625ff4969cb84d84f41137b2d91d
Aungkokolin1997 <aungkokolin23397@gmail.com>
2023-09-06 10:08:48 +0800

    adjustment

--- product_group_manage_product/__manifest__.py
+++ product_group_manage_product/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2023 Quartile Limited
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product Group Manage Product",
     "version": "11.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Product",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": ["product"],
     "data": [
         "security/product_group_manage_product_security.xml",
