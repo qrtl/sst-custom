ee69a5b6139a8d01bac40706788b532f033b563c
Aungkokolin1997 <lin@quartile.co>
2023-09-06 10:11:45 +0630

    update

--- account_security_adj/__manifest__.py
+++ account_security_adj/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2023 Quartile Limited
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Account Security Adj",
     "version": "11.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Accounting",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": ["account"],
     "data": ["views/account_invoice_views.xml"],
     "installable": True,
