7dd3e9cc9b626a83c5231619be6e715e6a74571f
Aungkokolin1997 <aungkokolin23397@gmail.com>
2023-09-07 16:07:39 +0800

    update

--- product_group_manage_product/readme/DESCRIPTION.rst
+++
@@ -1,1 +0,0 @@
-This module adds a new group (group_manage_product) with permissions for creating, editing, and deleting products.
--- product_group_product_manager/__manifest__.py
+++ product_group_product_manager/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Quartile Limited
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Product Group Manage Product",
+    "name": "Product Group Product Manager",
     "version": "11.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
