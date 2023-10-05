8e0718a94ad403596b89014776afbf1602bbbb7e
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-10-05 10:28:47 +0630

    [11.0][3810][REF] sale_ext_sst (#355)
    * [REF] sale_ext_sst

--- sale_order_invoice_residual/__manifest__.py
+++ sale_order_invoice_residual/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2017-2018 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Extension for sales functions",
-    "version": "11.0.1.2.1",
+    "name": "Sale Order Invoice Residual",
+    "version": "11.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Sale",
     "license": "AGPL-3",
-    "depends": ["sale_stock", "sale_order_dates"],
+    "depends": ["sale"],
     "data": ["views/sale_order_views.xml"],
     "installable": True,
 }
---
+++ sale_order_partner_phone/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2017-2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Partner Phone",
+    "version": "11.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["views/sale_order_views.xml"],
+    "installable": True,
+}
