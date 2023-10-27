f3c6d09fd80b90207315d1abf3ef37f6245040da
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-10-27 09:31:42 +0630

    [3625][ADD] product_group_product_manager (#351)
    * [ADD] product_group_manage_product

---
+++ product_group_product_manager/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Group Product Manager",
+    "version": "11.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Product",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "data": [
+        "security/product_group_manage_product_security.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
---
+++ product_group_product_manager/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds a new group (group_product_manager) with permissions for creating, editing, and deleting products.
