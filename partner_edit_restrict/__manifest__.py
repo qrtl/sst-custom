b22f937983fa8bb337422d218d562f67940cd3ee
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-12-06 14:32:39 +0630

    [4020][11.0][ADD] partner_edit_restrict: Migration to 11.0 (#365)
    Co-authored-by: kanda999 <kanda@quartile.co>
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>

---
+++ partner_edit_restrict/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Edit Restrict",
+    "category": "Partner",
+    "version": "11.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "security/partner_edit_restrict_security.xml",
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
---
+++ partner_edit_restrict/models/res_partner.py
@@ -0,0 +1,27 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, fields, models
+from odoo.exceptions import UserError
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    restrict_edit = fields.Boolean(
+        help="Enable to restrict the editing record for non-system users."
+    )
+
+    def write(self, values):
+        if (
+            self.env.user.has_group("partner_edit_restrict.group_partner_edit")
+            or self.env.user._is_superuser()
+        ):
+            return super().write(values)
+        restricted_recs = self.filtered(lambda x: x.restrict_edit)
+        if restricted_recs:
+            names = ", ".join(restricted_recs.mapped("name"))
+            raise UserError(
+                _("You are not allowed to modify following partner: %s") % (names)
+            )
+        return super().write(values)
