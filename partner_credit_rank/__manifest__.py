bb4d9665d2b7760d5c8d2979786fb82b05313c3a
Aungkokolin1997 <lin@quartile.co>
2023-12-12 08:56:19 +0630

    [ADD] partner_credit_rank

---
+++ partner_credit_rank/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Credit Rank",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Partner",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
---
+++ partner_credit_rank/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This module adds a credit_rank field to assign a rank based on
+creditworthiness.
