1711201e126c43f13736227ddf1eb056cdfe3df4
Aungkokolin1997 <aungkokolin23397@gmail.com>
2024-01-18 20:56:30 +0800

    [ADD] product_name_translate_false

---
+++ product_name_translate_false/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Name Translate False",
+    "version": "11.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Product",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "installable": True,
+}
---
+++ product_name_translate_false/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module disables the product name field translation, as a workaround for the performance issue
+described in https://github.com/odoo/odoo/issues/18679.
